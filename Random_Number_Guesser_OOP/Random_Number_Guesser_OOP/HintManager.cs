5cb7bb740bf282341d30a52a3ffa29d448f6456d
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-08-19 10:24:10 +0200

    add responses to answers

--- Random_Number_Guesser_OOP/Random_Number_Guesser_OOP/HintManager.cs
+++ Random_Number_Guesser_OOP/Random_Number_Guesser_OOP/HintManager.cs
@@ -8,5 +8,28 @@ namespace Random_Number_Guesser_OOP
 {
     internal class HintManager
     {
+        TryCounter tryCounter = new TryCounter();
+
+        public void OnRightAnswer()
+        {
+            int countOfTries = tryCounter.count;
+            Console.WriteLine("You guessed the number correctly. Well done!");
+            Console.WriteLine($"It took you {countOfTries} tries to guess it.\n");
+        }
+
+        public void OnWrongAnswer(int number, int currentInt)
+        {
+            if (number > currentInt)
+            {
+                Console.WriteLine("Your guess was greater than the secret number. Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Your guess below the the secret number. Try again.");
+            }
+
+            // counts up every time there's a wrong guess
+            tryCounter.CountUp();
+        }
     }
 }
