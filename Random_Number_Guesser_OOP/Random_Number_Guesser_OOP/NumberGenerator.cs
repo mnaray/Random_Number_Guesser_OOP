82a759ca4cf8f30f65f5baf3e10a20d7793c13d8
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-08-19 08:49:14 +0200

    write class number generator

--- Random_Number_Guesser_OOP/Random_Number_Guesser_OOP/NumberGenerator.cs
+++ Random_Number_Guesser_OOP/Random_Number_Guesser_OOP/NumberGenerator.cs
@@ -8,5 +8,17 @@ namespace Random_Number_Guesser_OOP
 {
     internal class NumberGenerator
     {
+        public int currentNumber { get; init; }
+
+        public NumberGenerator()
+        {
+            this.currentNumber = GenerateNumber();
+        }
+
+        private int GenerateNumber()
+        {
+            Random r = new Random();
+            return r.Next();
+        }
     }
 }
